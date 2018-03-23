5d1bcbe059ecc0049ac7194f33116b66d5d8c64a
Harkishen Singh <harkishensingh@hotmail.com>
2018-03-24 00:09:17 +0530

    enhanced features

--- decryptor.py
+++ decryptor.py
@@ -1,32 +1,27 @@
 import random
 
-
-
 class decryptor :
     
-    def assigning_key(self):
-        self.name = input('Enter the Encode file key Name : ')
-        self.name= self.name+ '.txt'
+    def assigning_key(self, data):
+        #self.name = input('Enter the Encode file key Name : ')
+        #self.name= self.name+ '.txt'
         self.ene = " "
-        fk = open(self.name, 'r')
+        fk = data
         self.stored_key = {}
 
         a = 0
 
-        for i in fk.readlines():
-            sub = i[0:8]
+        for i in fk.split(" "):
+            sub = i
             self.stored_key[sub] = chr(a + 33)
             a = a + 1
+        #print(self.stored_key)
 
-
-
-        print(self.stored_key)
-
-    def accessing_en_doc(self):
-        self.name_file = input('Enter the name of the file to be decrypted')
+    def accessing_en_doc(self, filename):
+        '''self.name_file = input('Enter the name of the file to be decrypted')
         self.name_file = self.name_file + '.txt'
-        self.f = open(self.name_file,'r')
-        self.text = self.f.read()
+        self.f = open(self.name_file,'r')'''
+        self.text = filename
         l = len(self.text)
 
         #conversion starts below  618_encryptCode_Ʀ   618__encrypted_doc_check2
@@ -34,24 +29,20 @@ class decryptor :
         sub = self.text.split(" ")
 
         for i in sub:
-            print(i)
+            #print(i)
             for j,k in self.stored_key.items() :
                 if j == i:
                     self.ene =self.ene + k
 
 
-        print(self.ene)
+        #print(self.ene)
+        #self.new_extracted_file()
 
     def new_extracted_file(self):
-        f2 = open('extracted_' + self.name_file[0:3]+'.txt', 'w')
-        f2.write(self.ene)
+        #f2 = open('extracted_' + self.name_file[0:3]+'.txt', 'w')
+        #f2.write(self.ene)
+        return self.ene
 
-
-
-obj = decryptor()
-obj.assigning_key()
-obj.accessing_en_doc()
-obj.new_extracted_file()
 
 
 
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for, send_file
 from app import Encryption
+from decryptor import decryptor
 
 object = Encryption()
 object.code_allocation2()
 app = Flask(__name__)
 
+object2 = decryptor()
 # server version for the Encrytion Engine
 
 @app.route('/') # defines the homepage
@@ -27,6 +29,20 @@ def received():
 		b, h = object.open_file(text)
 
 		return render_template('output.html', uff = b, decrypt = h)
+
+@app.route('/decryptor', methods=['GET','POST'])
+def decryptorCaller():
+	return render_template('decryptAsker.html')
+
+@app.route('/received2', methods=["POST"])
+def decryptionProcess():
+	if request.method == 'POST':
+		key = request.form['key']
+		object2.assigning_key(key)
+		object2.accessing_en_doc(request.form['en_code'])
+		got = object2.new_extracted_file()
+		#print(got)
+		return render_template('decryptedCode.html', message = got)
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', 5000, True)
